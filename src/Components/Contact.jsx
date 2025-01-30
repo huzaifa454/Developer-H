ae5c0a07d0e4222c3d58f9bdc8dfb939d04c6958
Huzaifa <huzaifaaamir454@gmail.com>
2025-01-31 01:54:43 +0500

    much better

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -7,17 +7,12 @@ const Contact = () => {
   return (
     <div
       id="contact"
-      className="min-h-screen flex flex-col justify-center items-center text-white cursor-default px-6 py-8"
+      className="min-h-screen flex flex-col justify-center items-center text-white cursor-default py-8"
     >
-      <motion.h1
-        className="text-primaryfont-serif text-primary text-6xl md:text-8xl font-extrabold relative"
-        initial={{ opacity: 0, y: -50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: false, amount: 0.3 }}
-        transition={{ duration: 1 }}
-      >
-        <span className="text-white">Get in</span>Touch
+      <motion.h1 className="text-primaryfont-serif text-primary text-5xl sm:text-6xl font-extrabold relative">
+        <span className="text-white">Get in</span> Touch
       </motion.h1>
+
       <p className="text-center text-lg mt-4 max-w-2xl">
         We are excited to collaborate and bring your ideas to life. Whether you
         have a question or want to discuss a potential project, feel free to
@@ -25,7 +20,6 @@ const Contact = () => {
       </p>
 
       <div className="flex flex-col md:flex-row items-center justify-between w-full max-w-5xl mt-12 space-y-8 md:space-y-0">
-        {/* Phone Section */}
         <div className="flex flex-col items-center w-full md:w-1/3 text-center">
           <FaPhoneAlt className="text-primary text-5xl mb-4" />
           <h2 className="text-xl font-bold">Phone</h2>
@@ -33,7 +27,6 @@ const Contact = () => {
           <p className="text-sm text-primary">Mon-Fri, 9am - 6pm</p>
         </div>
 
-        {/* Image Section */}
         <div className="w-full md:w-1/3 flex justify-center">
           <img
             src={pic}
@@ -42,7 +35,6 @@ const Contact = () => {
           />
         </div>
 
-        {/* Email Section */}
         <div className="flex flex-col items-center w-full md:w-1/3 text-center">
           <FaEnvelope className="text-primary text-5xl mb-4" />
           <h2 className="text-xl font-bold">Email</h2>
